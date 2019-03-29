27501ba10a3df715ef8493dfbec8182cff0b472b
chengjiang <15712859926@163.com>
2019-03-29 17:54:04 +0800

    尝试在react使用ajax

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import React, { Component } from 'react';
-import TodoActions from './actions/TodoActions';
-import TodoList from './components/TodoList';
-import Store from './store/TodoStore';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: []
-        }
-        this.addTodo = this.addTodo.bind(this);
-        this.refreshData = this.refreshData.bind(this);
-    }
     componentDidMount() {
-        Store.addChangeListener(this.refreshData);
-    }
-    refreshData() {
-        let listData = Store.getTodoListData();
-        this.setState({
-            list: listData
+        fetch({
+            method:'post',
+            url:"/api/index/index",
+            data:{
+                userid:'1',
+                sessionid:'n7b0km6figrarrfveo83c8ias1'
+            }
         })
-    }
-    addTodo(data) {
-        // 调用Action
-        TodoActions.addTodo(data);
+            .then(res => res.json())
+            .then((result) => {
+                console.log(result);
+            }, (error) => {
+                console.log(error);
+            })
     }
     render() {
-        return (
-            <div className="App">
-                <TodoList onClick={this.addTodo} list={this.state.list} />
-            </div>
-        )
+        return <div>
+
+        </div>
     }
 }
-
 export default App;
